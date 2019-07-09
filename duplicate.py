685b44194babeef7ece9e420c35f826f67da5228
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-09 12:32:00 +0530

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,9 +1,9 @@
 def Repeat(x):
-    _size = len(x)
+    size = len(x)
     repeated = []
-    for i in range(_size):
+    for i in range(size):
         k = i + 1
-        for j in range(k, _size):
+        for j in range(k, size):
             if x[i] == x[j] and x[i] not in repeated:
                 repeated.append(x[i])
     if(len(repeated)==0):
