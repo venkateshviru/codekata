339328c2556c45a5482a23f21316e1c3a74f5921
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-09 12:51:08 +0530

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,22 +1,22 @@
 def main():
-    def Repeat(x):
-    size = len(x)
+    n=int(input())
+    l=[]
+    l=list(input().split())
+    l.sort()
+    size = len(l)
     repeated = []
     for i in range(size):
         k = i + 1
         for j in range(k, size):
-            if x[i] == x[j] and x[i] not in repeated:
-                repeated.append(x[i])
+            if l[i] == l[j] and l[i] not in repeated:
+                repeated.append(l[i])
     if(len(repeated)==0):
         print("unique")
     else:
         for i in range (0,len(repeated)):
             print(repeated[i])
-n=int(input())
-l=[]
-l=list(input().split())
-l.sort()
-Repeat(l)
+
+
 
 if __name__ == '__main__':
     main()
