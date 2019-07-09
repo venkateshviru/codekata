1f6b4eadba62edb03e8fd49fd2070eb23f34e9fc
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-09 13:01:59 +0530

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,22 +1,2 @@
-def main():
-    n=int(input())
-    l=[]
-    l=list(map(int,input().split()))
-    l.sort()
-    size = len(l)
-    repeated = []
-    for i in range(size):
-        k = i + 1
-        for j in range(k, size):
-            if l[i] == l[j] and l[i] not in repeated:
-                repeated.append(l[i])
-    if(len(repeated)==0):
-        print("unique")
-    else:
-        for i in range (0,len(repeated)):
-            print(repeated[i])
-
-
-
-if __name__ == '__main__':
-    main()
+n=int(input())
+print(int((n*(n-1))/2))
