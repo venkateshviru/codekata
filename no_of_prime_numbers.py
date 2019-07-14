d2a6f70ffa12e72a96e23cb12ca212192c18bb89
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-13 21:57:13 -0700

    Create no_of_prime_numbers.py

---
+++ no_of_prime_numbers.py
@@ -0,0 +1,19 @@
+def main():
+    n = []
+    n = input().split()
+    l = n[0]
+    k = n[1]
+    l = int(l)
+    k = int(k)
+    res = []
+    for i in range(l,k + 1):
+        if i > 1:
+             for j in range(2,i):
+                 if ((i % j) == 0):
+                     break
+             else:
+                 res.append(i)
+    print(len(res))
+
+if __name__ == '__main__':
+    main()
