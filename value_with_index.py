b372e65981e1dffd4e07cdf0eb30ac0789e5ac29
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-14 06:32:43 -0700

    Create value_with_index.py

---
+++ value_with_index.py
@@ -0,0 +1,13 @@
+l = []
+l1 = []
+l = input().split()
+for i in range(len(l)):
+    l[i] = int(l[i])
+for i in range(len(l)):
+    if(l[i]%2==0):
+        if(i%2!=0):
+            l1.append(l[i])
+    else:
+        if(i%2==0):
+            l1.append(l[i])
+print(*l1, sep=" ")
