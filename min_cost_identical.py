7a50ff86559aeeb92b6da48952d7df864d4cf2b1
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-14 07:34:53 -0700

    Update min_cost_identical.py

--- min_cost_identical.py
+++ min_cost_identical.py
@@ -7,9 +7,26 @@ l1 = list(l[0])
 l2 = list(l[1])
 n1 = len(l1)
 n2 = len(l2)
-n = n2 - n1
-for i in range(n1):
-    if(l1[i]!=l2[i]):
-        l3.append(l2[i])
-res = len(l3)+n
-print(res)
+if(n1>n2):
+    high = n1
+    low = n2
+else:
+    high = n2
+    low = n1
+n = high - low
+try:
+    for i in range(len(l1)):
+        l1[i] = int(l1[i])
+    for i in range(len(l2)):
+        l2[i] = int(l2[i])
+    for i in range(low):
+        if(l1[i]!=l2[i]):
+            l3.append(l2[i])
+    res = len(l3) + n
+    print(res)
+except ValueError:
+    for i in range(low):
+        if(l1[i]!=l2[i]):
+            l3.append(l2[i])
+    res = len(l3) + n
+    print(res)
