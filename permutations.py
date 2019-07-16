833cb075018507ed6a3a957b9eec54d523fa7345
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-16 07:45:33 -0700

    Create permutations.py

---
+++ permutations.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+n = input()
+l = list(n)
+for i in range(len(l)):
+    l[i] = int(l[i])
+l1 = ["".join(map(str,i)) for i in permutations(l)]
+l2 = []
+for i in l1:
+    if i not in l2:
+        l2.append(i)
+for i in range(len(l2)):
+    print(l2[i])
+
