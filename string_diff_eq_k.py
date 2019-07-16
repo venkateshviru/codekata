da061a968efea13df35cf5d1ceb2b3f3e020ee24
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-16 09:36:17 -0700

    Update string_diff_eq_k.py

--- string_diff_eq_k.py
+++ string_diff_eq_k.py
@@ -1,13 +1,16 @@
 l = []
+l1 = []
+l2 = []
+l3 = []
 l = input().split()
-l1 = [list(l[0])]
-l2 = [list(l[1])]
+l1 = list(l[0])
+l2 = list(l[1])
 k = int(l[2])
-res = []
-for i in range(len(l1)):
-    if l1[i]!=l2[i]:
-        res.append(l2[i])
-if len(res)==k:
+n = len(l1)
+for i in range(n):
+    if(l1[i]!=l2[i]):
+        l3.append(l2[i])
+if len(l3)==k:
     print("yes")
 else:
     print("no")
