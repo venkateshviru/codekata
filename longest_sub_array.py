08ff1d690707435f94ebbfd2da64554f91718e78
venkateshviru <36430114+venkateshviru@users.noreply.github.com>
2019-07-27 18:11:26 +0530

    Update longest_sub_array.py

--- longest_sub_array.py
+++ longest_sub_array.py
@@ -6,9 +6,9 @@ l = 1
 for i in range(1, n):
     if arr[i] > arr[i-1]:
         l = l+1
-    elif m < l:
-        m = l
     else:
+        if m < l:
+            m = l
         l = 1
 if m < l:
     m = l
